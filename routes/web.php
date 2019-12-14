bbe5099a9bbca8378b72ee5d07a7d7ba78501430
larozox <samisua16@gmail.com>
2019-12-13 21:36:03 -0500

    retornar vistas

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,5 @@
 <?php
 
-Route::get('contactanos',function(){ // contatanos es el nombre de la URL
-	return "Seccion de contactos";
-})->name('contactos'); // Se accede a la ruta pór medio del nombre
-
 Route::get('/',function(){
-	echo "<a href=".route('contactos').">Contatos 1 </a><br>";
-	echo "<a href=".route('contactos').">Contatos 1 </a><br>";
-	echo "<a href=".route('contactos').">Contatos 1 </a><br>";
-	echo "<a href=".route('contactos').">Contatos 1 </a><br>";
-	echo "<a href=".route('contactos').">Contatos 1 </a><br>";
-});
+	 return view('home');
+})->name('home');
