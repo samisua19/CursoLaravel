91015d73c98ed1b59632ace5886aa21c0796a78e
larozox <samisua16@gmail.com>
2019-12-13 23:48:30 -0500

    retornar vista desde portfoliocontroller

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,5 @@
 Route::view('/','home')->name('home');
 Route::view('/about','about')->name('about');
 //Route::view('/portfolio','portfolio',compact('portfolio'))->name('portfolio');
-Route::get('/portfolio','PortfolioController')->name('portfolio'); // se cambia por la de arriba
+Route::get('/portfolio','PortfolioController@index')->name('portfolio'); // se cambia por la de arriba
 Route::view('/contact','contact')->name('contact');
