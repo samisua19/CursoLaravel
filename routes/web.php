3cc0509d0e4178fc2d328b9241604fa458294ca5
larozox <samisua16@gmail.com>
2019-12-13 23:36:09 -0500

    Crear y añadir controlador de protfolio

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,10 @@
 
 //Route::view('/','home',['nombre'=>'Jorge']); // cuando se retornan vistas con pocas o ninguna informacion
 
-$portfolio = [
-	['title' => 'Proyecto #1'],
-	['title' => 'Proyecto #4'],
-	['title' => 'Proyecto #3'],
-	['title' => 'Proyecto #4']
-];
+
 
 Route::view('/','home')->name('home');
 Route::view('/about','about')->name('about');
-Route::view('/portfolio','portfolio',compact('portfolio'))->name('portfolio');
+//Route::view('/portfolio','portfolio',compact('portfolio'))->name('portfolio');
+Route::get('/portfolio','PortfolioController')->name('portfolio'); // se cambia por la de arriba
 Route::view('/contact','contact')->name('contact');
