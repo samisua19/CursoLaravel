994bf9fb48b4f010dbd5c43b5378605284347615
larozox <samisua16@gmail.com>
2019-12-13 22:01:55 -0500

    Inicio de navegacion entre vistas

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,14 @@
 	<title>Home</title>
 </head>
 <body>
+	<nav>
+		<ul>
+			<li><a href="/">Home</a></li>
+			<li><a href="/about">About</a></li>
+			<li><a href="/portfolio">Portfolio</a></li>
+			<li><a href="/contact">Contact</a></li>
+		</ul>
+	</nav>
 <h1>Home</h1>
 Bienveni@ <?php echo $nombre ?? "Invitado" ?>
 </body>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
-Route::view('/','home',['nombre'=>'Jorge']); // cuando se retornan vistas con pocas o ninguna informacion
+//Route::view('/','home',['nombre'=>'Jorge']); // cuando se retornan vistas con pocas o ninguna informacion
 
-
+Route::view('/','home')->name('home');
+Route::view('/about','about')->name('about');
+Route::view('/portfolio','portfolio')->name('portfolio');
+Route::view('/contact','contact')->name('contact');
