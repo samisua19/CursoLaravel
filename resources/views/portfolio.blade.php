e09d92189428f93df5d31de7d9eb6513b6f4e9ea
larozox <samisua16@gmail.com>
2019-12-13 23:16:01 -0500

    Estructuras de control con blade

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -5,4 +5,15 @@
 @section('content') {{--inicio de contenido dinamico--}}
  	{{-- Donde va el contenido dinamico --}}
  	<h1>Portfolio</h1>
+ 	<ul>
+ 		{{-- @if @isset @foreach @forelse(foreach con condicional si la variable esta vacia) @for @while --}}
+ 			@forelse($portfolio as $portfolioitem)
+ 				{{-- <li>{{ $portfolioitem['title']}} <pre>{{ var_dump($loop)}}</pre> </li>  var_dump($loop) muestra la informacion de la iteracion de for
+ 				(i teracion,index,elemtos que quedan, total de elementos, si es el primero, si es el ultimo,profundidad del arreglo)--}}
+ 				<li>{{ $portfolioitem['title']}} <pre>{{ $loop->last ? 'Es el ultimo' : ''}}</pre> </li> {{-- pregunta si el item es el ultimo--}}
+ 			@empty
+ 				<li>No hay proyectos para mostrar</li>
+ 			@endforelse
+
+ 	</ul>
 @endsection {{--fin de contenido dinamico--}}
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,14 @@
 
 //Route::view('/','home',['nombre'=>'Jorge']); // cuando se retornan vistas con pocas o ninguna informacion
 
+$portfolio = [
+	['title' => 'Proyecto #1'],
+	['title' => 'Proyecto #4'],
+	['title' => 'Proyecto #3'],
+	['title' => 'Proyecto #4']
+];
+
 Route::view('/','home')->name('home');
 Route::view('/about','about')->name('about');
-Route::view('/portfolio','portfolio')->name('portfolio');
+Route::view('/portfolio','portfolio',compact('portfolio'))->name('portfolio');
 Route::view('/contact','contact')->name('contact');
