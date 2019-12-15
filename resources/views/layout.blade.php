9231c69fb10cd5ed06b090b46e6f66359b3a80a1
larozox <samisua16@gmail.com>
2019-12-14 21:58:45 -0500

    Formas de obtener la url de la pagina

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,11 +2,21 @@
 <html>
 <head>
 	<title>@yield('title','Aprendible')</title> {{-- El segundo parametro es por defecto, si no esta definido--}}
+	<style>
+		.active a {
+			color: red;
+			text-decoration: none;
+		}
+	</style>
 </head>
 <body>
 	<nav>
+
+		{{-- {{request()->url()}} muestra la url de la pagina donde esta
+		{{ request()->path()}} muestra la url interna de la pagina
+		{{ request()-routeIs('home') }} devuelve V/F si la pagina por parametro es la misma--}}
 		<ul>
-			<li><a href="/">Home</a></li>
+			<li class="active"><a href="/">Home</a></li>
 			<li><a href="/about">About</a></li>
 			<li><a href="/portfolio">Portfolio</a></li>
 			<li><a href="/contact">Contact</a></li>
